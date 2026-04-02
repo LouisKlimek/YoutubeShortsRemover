c05c66c6d00532067a18a09410f6432080b25474
LouisKlimek <louisklimek@gmail.com>
2026-04-02 16:31:54 +0200

    Hide shorts on video play page, channel page and search results

--- shorts-remover.js
+++ shorts-remover.js
@@ -28,6 +28,44 @@ function hideShorts() {
         }
     }
 
+    // Hide Shorts on the video page
+    // <ytm-shorts-lockup-view-model-v2>
+    const potentialShortsVideoPageElems = document.querySelectorAll("ytm-shorts-lockup-view-model-v2");
+    for(const elem of potentialShortsVideoPageElems) {
+        elem.style.display = "none";
+    }
+
+    const potentialShortsVideoPageElems2 = document.querySelectorAll("ytm-shorts-lockup-view-model-v3");
+    for(const elem of potentialShortsVideoPageElems2) {
+        elem.style.display = "none";
+    }
+
+    // Remove shorts on channel page
+    const potentialShortsChannelPageElems = document.querySelectorAll("div.ytd-reel-shelf-renderer");
+    for(const elem of potentialShortsChannelPageElems) {
+        console.log("Checking potential shorts section on channel page");
+        elem.style.display = "none";
+    }
+
+    // Hide Shorts tab on channel page
+    const potentialShortsChannelPageTabElems = document.querySelectorAll(".yt-tab-shape__tab");
+    for(const elem of potentialShortsChannelPageTabElems) {
+        if(elem.innerText == "Shorts") {
+            // Get Parent of the tab element, which is the actual tab button. its a <yt-tab-shape> element
+            const parentElem = elem.parentElement;
+            parentElem.style.display = "none";
+        }
+    }
+
+    // Hide shorts in search results
+    //style-scope yt-chip-cloud-renderer iron-selected
+    const potentialShortsSearchResultElems = document.querySelectorAll("yt-chip-cloud-chip-renderer");
+    for(const elem of potentialShortsSearchResultElems) {
+        if(elem.innerText == "Shorts") {
+            elem.style.display = "none";
+        }
+    }
+
 }
 
 // Run the function on script load
